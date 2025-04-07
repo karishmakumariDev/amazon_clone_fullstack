40fae47a97a27f60cbf3d8ac0101f4145eab41aa
karishmakumariDev <kumarikarishma311329@gmail.com>
2025-04-07 11:08:02 +0530

    create file within home one product.jsx and another slide.jsx than fill will be complited

--- frontend/src/components/home/Maincamp.jsx
+++ frontend/src/components/home/Maincamp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import './Home.css';
+import Slide from './Slide';
 
 const Maincamp = () => {
   return (
     <div className='home_section'>
         <div>
             <Banner/>
+            <Slide/>
         </div>
     </div>
   )
